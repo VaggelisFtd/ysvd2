d930af72b47fb2cf9d35ecf569c92bb8003f4307
VaggelisFtd <vaggelisfotiadis78@gmail.com>
2023-11-13 20:20:12 +0200

    test

--- src/hash_file.c
+++ src/hash_file.c
@@ -6,42 +6,49 @@
 #include "hash_file.h"
 #define MAX_OPEN_FILES 20
 
-#define CALL_BF(call)       \
-{                           \
-  BF_ErrorCode code = call; \
-  if (code != BF_OK) {         \
-    BF_PrintError(code);    \
-    return HP_ERROR;        \
-  }                         \
-}
+#define CALL_BF(call)         \
+  {                           \
+    BF_ErrorCode code = call; \
+    if (code != BF_OK)        \
+    {                         \
+      BF_PrintError(code);    \
+      return HP_ERROR;        \
+    }                         \
+  }
 
-HT_ErrorCode HT_Init() {
-  //insert code here
+HT_ErrorCode HT_Init()
+{
+  // insert code here
+  // test push
   return HT_OK;
 }
 
-HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
-  //insert code here
+HT_ErrorCode HT_CreateIndex(const char *filename, int depth)
+{
+  // insert code here
   return HT_OK;
 }
 
-HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc){
-  //insert code here
+HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc)
+{
+  // insert code here
   return HT_OK;
 }
 
-HT_ErrorCode HT_CloseFile(int indexDesc) {
-  //insert code here
+HT_ErrorCode HT_CloseFile(int indexDesc)
+{
+  // insert code here
   return HT_OK;
 }
 
-HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) {
-  //insert code here
+HT_ErrorCode HT_InsertEntry(int indexDesc, Record record)
+{
+  // insert code here
   return HT_OK;
 }
 
-HT_ErrorCode HT_PrintAllEntries(int indexDesc, int *id) {
-  //insert code here
+HT_ErrorCode HT_PrintAllEntries(int indexDesc, int *id)
+{
+  // insert code here
   return HT_OK;
 }
-

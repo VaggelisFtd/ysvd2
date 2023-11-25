59c1481d36d577e795f2f7a548ea76f6f70289a4
VaggelisPapako <papakostev@gmail.com>
2023-11-25 12:48:06 +0200

    new condition for Inser algorithm that may be wrong Added InsertEntry copy to try the actual implementation

--- src/hash_file.c
+++ src/hash_file.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h> // for debugging
 
 #include "bf.h"
 #include "../include/hash_file.h"
@@ -44,6 +45,7 @@ HT_ErrorCode HT_CloseFile(int indexDesc) {
   return HT_OK;
 }
 
+/*
 HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) { // mporoume na ths pername k epipleon orismata ???
   
   char* data;
@@ -75,7 +77,97 @@ HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) { // mporoume na ths p
   // printf(" ht_block_info.max_records = %d\n", ht_block_info.max_records);
 
   // if there is enough space for the record
-  // // if (ht_block_info.num_records < ht_info->max_records) { /*prepei mallon na balw to "global" max records tou bucket/block edw (an nai prepei na to orisw) -> to size opws to lew an 8umamai kala */
+  // // if (ht_block_info.num_records < ht_info->max_records) { // prepei mallon na balw to "global" max records tou bucket/block edw (an nai prepei na to orisw) -> to size opws to lew an 8umamai kala 
+  if (ht_block_info.num_records < ht_block_info.max_records) {
+    // insert the record in the last position (for records) in the block
+    printf(" record.id = %d\n", record.id);
+    printf(" record.name = %s\n", record.name);
+    printf(" record.surname = %s\n", record.surname);
+    printf(" record.city = %s\n", record.city);
+    memcpy(data + sizeof(Record) * ht_block_info.num_records, &record, sizeof(Record));
+
+    // this block's data changed, so we update its hp_block_indo
+    // maybe more have to change here
+    ht_block_info.num_records++;
+
+    // copy the updated hp_info at the end of the block
+    memcpy(data + BF_BLOCK_SIZE - sizeof(HT_block_info), &ht_block_info, sizeof(HT_block_info));
+
+    // write the block back on disc
+    BF_Block_SetDirty(block);
+    if ((BF_UnpinBlock(block)) < 0) {
+      printf("Error unpinning block in HT_InsertEntry\n");
+      return -1;
+    }
+  }
+  // else {
+  //   // there is not enough space
+  //   if ((BF_UnpinBlock(block)) < 0) {
+  //     printf("Error unpinning block in HT_InsertEntry\n");
+  //     return -1;
+  //   }
+
+  //   // we need to allocate a new block, so we get out of this If
+  //   // new_block_needed = true;
+  // }
+
+  BF_Block_Destroy(&block);
+
+  return HT_OK;
+}
+*/
+
+HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) {
+  
+  char* data;
+  BF_Block* block;
+  HT_info ht_info;
+  HT_block_info ht_block_info;
+  BF_Block_Init(&block);
+  if ((BF_GetBlock(indexDesc, 0, block)) < 0) {
+    printf("Error getting block in HT_InsertEntry\n");
+    return -1;
+  }
+
+  // get pointer to block's 0 data
+  data = BF_Block_GetData(block);
+  // get the metadata of this block so that we can access ht_info.fileDesc
+  memcpy(&ht_info, data, sizeof(HT_info));
+
+  // In which bucket to insert
+  int bucket_to_insert = hash2(record.id, ht_info.buckets);
+  if(bucket_to_insert < 0 || bucket_to_insert >= ht_info.buckets)
+      return -1;
+  assert(bucket_to_insert >= 0 && bucket_to_insert < ht_info.buckets);
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if ((BF_GetBlock(ht_info.fileDesc, 1, block)) < 0) {
+    printf("Error getting block in HT_InsertEntry\n");
+    return -1;
+  }
+  // get pointer to block's 1 data
+  data = BF_Block_GetData(block);
+  memcpy(&ht_block_info, data + BF_BLOCK_SIZE - sizeof(HT_block_info), sizeof(HT_block_info));
+
+
+  // if there is enough space for the record
+  // // if (ht_block_info.num_records < ht_info->max_records /* den einai orismeno akomh */ ) {
   if (ht_block_info.num_records < ht_block_info.max_records) {
     // insert the record in the last position (for records) in the block
     printf(" record.id = %d\n", record.id);
